ecacc39592cb1fe5744ee5c22fe0cc693c4ceaea
Yuu Kobayashi <kb10uy@kb10uy.org>
2019-04-07 13:24:55 +0900

    テンプレートを改変

---
+++ resources/views/components/includes.blade.php
@@ -0,0 +1,3 @@
+<link rel="stylesheet" href="{{ mix('/styles/app.css') }}">
+<script defer src="{{ mix('/scripts/vendor.js') }}"></script>
+<script defer src="{{ mix('/scripts/app.js') }}"></script>
---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        @include('components.includes')
+        <title>@yield('title') - ShortStoryServer</title>
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>
