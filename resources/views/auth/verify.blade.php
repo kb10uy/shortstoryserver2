12d4113530e2ce23c831e4d5427a106794677ff1
Yuu Kobayashi <kb10uy@kb10uy.org>
2019-04-08 20:54:41 +0900

    認証ページ

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -2,23 +2,17 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
-
-                <div class="card-body">
-                    @if (session('resent'))
-                        <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
-                        </div>
-                    @endif
-
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }}, <a href="{{ route('verification.resend') }}">{{ __('click here to request another') }}</a>.
-                </div>
-            </div>
+    @if (session('resent'))
+        <div class="alert alert-success" role="alert">
+            {{ __('A fresh verification link has been sent to your email address.') }}
         </div>
-    </div>
+    @endif
+
+    <h1>確認メールを送信しました</h1>
+    <p>
+        メールアドレスの有効性を確認するため、入力されたメールアドレスに確認用のリンクを送信しました。
+        この画面を閉じる前に、メールをご確認ください。
+    </p>
+    <a href="{{ route('verification.resend') }}">再送信する</a>
 </div>
 @endsection
