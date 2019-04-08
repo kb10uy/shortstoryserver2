0c0ceac492d53b217167c1a980e14d2c9e97d277
Yuu Kobayashi <kb10uy@kb10uy.org>
2019-04-08 20:54:34 +0900

    reCAPTCHA v3 を導入

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use App\Rules\Recaptcha3;
 
 class RegisterController extends Controller
 {
@@ -52,6 +53,7 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'recaptcha_token' => ['required', new Recaptcha3(0.5)],
         ]);
     }
 
---
+++ app/Rules/Recaptcha3.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Rules;
+
+use Exception;
+use GuzzleHttp\Client;
+use Illuminate\Contracts\Validation\Rule;
+
+class Recaptcha3 implements Rule
+{
+    /** @var Client client */
+    private $client;
+
+    /** @var float bot と判定する score の閾値 */
+    private $threshold;
+
+    /**
+     * Create a new rule instance.
+     *
+     * @param float $threshold 閾値
+     * @return void
+     */
+    public function __construct(float $threshold)
+    {
+        $this->client = new Client();
+        $this->threshold = $threshold;
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        // もらったトークンで検証
+        try {
+            $response = $this->client->post('https://www.google.com/recaptcha/api/siteverify', [
+                'form_params' => [
+                    'secret' => env('RECAPTCHA3_SECRET_KEY'),
+                    'response' => $value,
+                ],
+            ]);
+            $data = json_decode($response, true);
+            return $data['success'] && $data['score'] >= $this->threshold;
+        } catch(Exception $ex) {
+            return false;
+        }
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'The reCAPTCHA verification failed.';
+    }
+}
