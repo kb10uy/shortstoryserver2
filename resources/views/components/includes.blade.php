673fbdf0f6bfa3c2601fed0699079bb538ac6771
Yuu Kobayashi <kb10uy@kb10uy.org>
2019-08-29 23:31:07 +0900

    投稿表示画面のアイコン表示

--- resources/views/components/includes.blade.php
+++ resources/views/components/includes.blade.php
@@ -4,5 +4,6 @@
     integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf"
     crossorigin="anonymous">
 <link rel="stylesheet" href="{{ mix('/styles/app.css') }}">
+<link rel="icon" href="/favicon.ico">
 <script defer src="{{ mix('/scripts/vendor.js') }}"></script>
 <script defer src="{{ mix('/scripts/app.js') }}"></script>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,7 +7,7 @@
     <div class="post-info">
         <h1>{{ $title }}</h1>
         <div class="user">
-            <img src="https://via.placeholder.com/64" alt="{{ $author->name }}">
+            <img src="{{ $author->avatar_url }}" alt="{{ $author->name }}">
             <div>
                 Author:<br>
                 <a href="{{ route('users.show', ['name' => $author->name]) }}">{{ $author->name }} <small>&#64;{{ $author->name }}</small></a>
