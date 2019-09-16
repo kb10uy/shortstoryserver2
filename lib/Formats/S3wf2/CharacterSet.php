1a5447b30fba95fc58c7bce9252a4f7212c50dda
Yuu Kobayashi <kb10uy@kb10uy.org>
2019-09-16 11:21:10 +0900

    カスタム色指定を自動でダークモード対応する仕組みを ついか

--- lib/Formats/S3wf2/CharacterSet.php
+++ lib/Formats/S3wf2/CharacterSet.php
@@ -85,6 +85,110 @@ class CharacterSet
             $node->addTextNode(".custom-$color { color: #$color; }" . PHP_EOL);
         }
 
+        $node->addTextNode('@media (prefers-color-scheme: dark) {');
+        foreach ($this->customColors as $color) {
+            $inverted = $this->invertColorCode($color);
+            $node->addTextNode("  .custom-$color { color: #$inverted; }" . PHP_EOL);
+        }
+        $node->addTextNode('}');
+
         return $node;
+    }
+
+    /**
+     * ダークモード用に、明度を反転する。
+     *
+     * @return string 反転したカラーコード
+     */
+    private function invertColorCode(string $color): string
+    {
+        $original = strlen($color) === 6 ? $color : "{$color[0]}{$color[0]}{$color[1]}{$color[1]}{$color[2]}{$color[2]}";
+        $red = hexdec(substr($original, 0, 2)) / 255.0;
+        $green = hexdec(substr($original, 2, 2)) / 255.0;
+        $blue = hexdec(substr($original, 4, 2)) / 255.0;
+
+        [$hue, $saturation, $brightness] = $this->convertRgbToHsb([$red, $green, $blue]);
+        [$red, $green, $blue] = $this->convertHsbToRgb([$hue, $saturation, $brightness]);
+
+        $f2h = function (float $value) {
+            $intValue = (int) ($value * 255.0);
+            return str_pad(dechex($intValue), 2, '0');
+        };
+
+        return $f2h($red) . $f2h($green) . $f2h($blue);
+    }
+
+    private function convertRgbToHsb(array $rgbFloat): array
+    {
+        [$red, $green, $blue] = $rgbFloat;
+        $max = $red > $green ? $red : $green;
+        $max = $blue > $max ? $blue : $max;
+        $min = $red < $green ? $red : $green;
+        $min = $blue < $min ? $blue : $min;
+
+        $hue = $max - $min;
+        if ($hue > 0.0) {
+            if ($max === $red) {
+                $hue = ($green - $blue) / $hue;
+                if ($hue < 0.0) {
+                    $hue += 6.0;
+                }
+            } else if ($max === $green) {
+                $hue = 2.0 + ($blue - $red) / $hue;
+            } else {
+                $hue = 4.0 + ($red - $green) / $hue;
+            }
+        }
+        $hue /= 6.0;
+
+        $saturation = $max - $min;
+        if ($max != 0.0) {
+            $saturation /= $max;
+        }
+
+        $brightness = $max;
+
+        return [$hue, $saturation, $brightness];
+    }
+
+    public function convertHsbToRgb(array $hsbFloat): array
+    {
+        [$hue, $saturation, $brightness] = $hsbFloat;
+        [$red, $green, $blue] = [$brightness, $brightness, $brightness];
+
+        if ($saturation > 0.0) {
+            $hue *= 6.0;
+            $area = (int) $hue;
+            $rest = $hue - (float) $area;
+
+            switch ($area) {
+                case 0:
+                    $green *= 1.0 - $saturation * (1.0 - $rest);
+                    $blue *= 1.0 - $saturation;
+                    break;
+                case 1:
+                    $red *= 1.0 - $saturation * $rest;
+                    $blue *= 1.0 - $saturation;
+                    break;
+                case 2:
+                    $red *= 1.0 - $saturation;
+                    $blue *= 1.0 - $saturation * (1.0 - $rest);
+                    break;
+                case 3:
+                    $red *= 1.0 - $saturation;
+                    $green *= 1.0 - $saturation * $rest;
+                    break;
+                case 4:
+                    $red *= 1.0 - $saturation * (1.0 - $rest);
+                    $green *= 1.0 - $saturation;
+                    break;
+                case 5:
+                    $green *= 1.0 - $saturation;
+                    $blue *= 1.0 - $saturation * $rest;
+                    break;
+            }
+        }
+
+        return [$red, $green, $blue];
     }
 }
