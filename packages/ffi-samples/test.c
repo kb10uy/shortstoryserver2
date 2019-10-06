17a88d068e9803237f9d71e531d8a0442d1e869c
Yuu Kobayashi <kb10uy@kb10uy.org>
2019-10-06 21:20:34 +0900

    サンプルを追加

---
+++ packages/ffi-samples/test.c
@@ -0,0 +1,38 @@
+/*
+ * Compile command (example):
+ * gcc -std=c11 -L../s3wf2-rs/target/release -ls3wf2 test.c
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "../s3wf2-rs/include/s3wf2.h"
+
+const char *source =
+    ":character male kb10uy kb10uy\n"
+    ":character female natsuki 夏稀\n"
+    "@kb10uy 「ついに僕たちも [m C 言語]に進出したよ」\n"
+    "@natsuki 「やったッスね先パイ！！」\n";
+
+int main(void) {
+    // create new parser
+    s3wf2_parser *parser = s3wf2_parser_new();
+
+    // parse the S3WF2 text...
+    s3wf2_parser_parse(parser, source);
+
+    // get the formatted HTML
+    char *html = NULL;
+    s3wf2_emit_html(parser, &html);
+
+    // after use of returned HTML, we should free the buffer,
+    // as it is allocated in library
+    if (html) {
+        puts(html);
+        free(html);
+    }
+
+    // we should release the parser
+    s3wf2_parser_free(parser);
+
+    return 0;
+}
