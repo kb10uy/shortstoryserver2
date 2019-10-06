98436395df65fa718ecbdb6fffd6ada18d1ec131
Yuu Kobayashi <kb10uy@kb10uy.org>
2019-10-06 21:06:40 +0900

    HTML が吐けるはずです

--- packages/s3wf2-rs/include/s3wf2.h
+++ packages/s3wf2-rs/include/s3wf2.h
@@ -1,10 +1,32 @@
 #ifndef S3WF2_H
 #define S3WF2_H
 
+#include <stddef.h>
+
 struct s3wf2_parser_t;
+
+enum s3wf2_status_t {
+    S3WF2_SUCCESS = 0,
+    S3WF2_INVALID,
+    S3WF2_INVALID_SOURCE,
+    S3WF2_PARSE_ERROR,
+};
+
+/**
+ * Represents status of S3WF2 parser.
+ */
+typedef enum s3wf2_status_t s3wf2_status;
+
+/**
+ * An opaque struct of S3WF2 parser.
+ */
 typedef struct s3wf2_parser_t s3wf2_parser;
 
 s3wf2_parser *s3wf2_parser_new(void);
 void s3wf2_parser_free(s3wf2_parser *parser);
+s3wf2_status s3wf2_parser_reset(s3wf2_parser *parser);
+s3wf2_status s3wf2_parser_parse(s3wf2_parser *parser, const char *source);
+int s3wf2_parser_next_error(s3wf2_parser *parser, char *buffer, size_t buffer_length);
+s3wf2_status s3wf2_emit_html(const s3wf2_parser *parser, char **buffer);
 
 #endif
--- packages/s3wf2-rs/src/foreign.rs
+++ packages/s3wf2-rs/src/foreign.rs
@@ -1,12 +1,17 @@
-use std::ffi::CStr;
-use libc::{c_char, c_int};
 use crate::{
-    parser::Parser,
     document::Document,
+    emitter::{html::HtmlEmitter, Emit},
     error::Error,
+    parser::Parser,
+};
+use libc::{c_char, c_int, c_uchar, size_t};
+use std::{
+    ffi::{CStr, CString},
+    mem::forget,
+    io::Write,
+    slice::from_raw_parts_mut,
 };
 
-/// C API の処理結果を表す enum
 #[repr(C)]
 pub enum Status {
     Success = 0,
@@ -15,7 +20,6 @@ pub enum Status {
     ParseError,
 }
 
-/// C API で取り回すための [Parser] とゆかいな仲間たち
 pub struct ParserWrapper<'a> {
     parser: Parser,
     parse_result: Option<Result<Document<'a>, Vec<Error>>>,
@@ -30,19 +34,22 @@ impl<'a> ParserWrapper<'a> {
             errors_position: None,
         }
     }
+
+    fn reset(&mut self) {
+        self.parse_result = None;
+        self.errors_position = None;
+    }
 }
 
-/// 新しい ParserWrapper の
 #[no_mangle]
-pub extern fn s3wf2_parser_new<'a>() -> *mut ParserWrapper<'a> {
+pub extern "C" fn s3wf2_parser_new<'a>() -> *mut ParserWrapper<'a> {
     let parser = Box::new(ParserWrapper::new());
 
     Box::into_raw(parser)
 }
 
-
 #[no_mangle]
-pub unsafe extern fn s3wf2_parser_free(parser: *mut ParserWrapper) {
+pub unsafe extern "C" fn s3wf2_parser_free(parser: *mut ParserWrapper) {
     if parser.is_null() {
         return;
     }
@@ -50,7 +57,21 @@ pub unsafe extern fn s3wf2_parser_free(parser: *mut ParserWrapper) {
 }
 
 #[no_mangle]
-pub unsafe extern fn s3wf2_parser_parse(parser: *mut ParserWrapper, source: *const c_char) -> Status {
+pub unsafe extern "C" fn s3wf2_parser_reset(parser: *mut ParserWrapper) -> Status {
+    if parser.is_null() {
+        return Status::Invalid;
+    }
+    let parser: &mut ParserWrapper = &mut *parser;
+
+    parser.reset();
+    Status::Success
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn s3wf2_parser_parse(
+    parser: *mut ParserWrapper,
+    source: *const c_char,
+) -> Status {
     if parser.is_null() {
         return Status::Invalid;
     }
@@ -74,16 +95,68 @@ pub unsafe extern fn s3wf2_parser_parse(parser: *mut ParserWrapper, source: *con
 }
 
 #[no_mangle]
-pub unsafe extern fn s3wf2_parser_next_error(parser: *mut ParserWrapper) -> c_int {
+pub unsafe extern "C" fn s3wf2_parser_next_error(
+    parser: *mut ParserWrapper,
+    buffer: *mut c_char,
+    buffer_length: size_t,
+) -> c_int {
     if parser.is_null() {
         return 0;
     }
     let parser: &mut ParserWrapper = &mut *parser;
 
+    let errors = match &parser.parse_result {
+        Some(Err(errors)) => errors,
+        _ => return 0,
+    };
+
     match parser.errors_position {
         Some(next) => {
-            1
+            if next < errors.len() {
+                let mut sized_buffer = from_raw_parts_mut(buffer as *mut c_uchar, buffer_length);
+                let message = format!("{}", errors[next]);
+                match sized_buffer.write(&message.as_bytes()) {
+                    Ok(_) => {
+                        parser.errors_position = Some(next + 1);
+                        1
+                    }
+                    Err(_) => {
+                        parser.errors_position = None;
+                        0
+                    }
+                }
+            } else {
+                parser.errors_position = None;
+                0
+            }
         }
-        None => 0
+        None => 0,
+    }
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn s3wf2_emit_html(
+    parser: *const ParserWrapper,
+    result_ptr: *mut *const c_char,
+) -> Status {
+    if parser.is_null() {
+        return Status::Invalid;
+    }
+    let parser: &ParserWrapper = &*parser;
+
+    let document = match &parser.parse_result {
+        Some(Ok(document)) => document,
+        _ => return Status::Invalid,
+    };
+    let mut html_buffer: Vec<u8> = Vec::with_capacity(1 << 16);
+    let emitter = HtmlEmitter::new(4);
+    match emitter.emit(&mut html_buffer, document) {
+        Ok(()) => {
+            let result = CString::from_vec_unchecked(html_buffer);
+            *result_ptr = result.as_ptr();
+            forget(result);
+            Status::Success
+        }
+        Err(_) => Status::Invalid,
     }
 }
